f49c4a5987ce992b9819294c16aa48568a480994
Denzell Ford <fdenzell@gmail.com>
2020-11-08 20:13:45 -0600

    typos

--- server/src/app.py
+++ server/src/app.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 def create_app(config_object='src.settings') -> Flask:
 
-    app = Flask()
+    app = Flask(__name__)
     app.config.from_object(config_object)
     init_extensions(app)
 
--- server/src/ext.py
+++ server/src/ext.py
@@ -1,4 +1,4 @@
-rom flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
