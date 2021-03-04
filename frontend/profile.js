60a438a742b53d8b2f7a08a01cebada2fc6a8a86
Sarim Aleem <sarimaleem99@gmail.com>
2021-03-03 21:05:54 -0600

    some changes to pfp page

--- frontend/profile.js
+++ frontend/profile.js
@@ -1,22 +1,27 @@
 var info = {
   first: "Bob",
   last: "Bill",
-  bio: "hello, my name is bob the builder and I am from pakistan. I really like making buildings. In my free time I read books and don't study but also watch a lot of tv. I really like food because every one likes food. One of my hobbies is that I really like to bake which is always somewhat nice I suppose",
+  bio:
+    "hello, my name is bob the builder and I am from pakistan. I really like making buildings. In my free time I read books and don't study but also watch a lot of tv. I really like food because every one likes food. One of my hobbies is that I really like to bake which is always somewhat nice I suppose",
 };
 
 var socials = {
-    "discord" : "bob#123",
-    "facebook" : "bobthebuilder",
-    "snapchat" : "bobthebuilderyeswecan"
-}
+  discord: "bob#123",
+  facebook: "bobthebuilder",
+  snapchat: "bobthebuilderyeswecan",
+};
 
 document.getElementById("name").innerText = info.first + " " + info.last;
-document.getElementById("bio").value = info.bio
+document.getElementById("bio").value = info.bio;
 
 for (var key of Object.keys(socials)) {
-  let obj = document.createElement("p");
-  obj.innerHTML = key + ": " + socials[key];
-  document.getElementById("socialsList").appendChild(obj);
+  let socialContainer = document.createElement("div");
+  socialContainer.innerHTML = key + " " + socials[key];
+  let button = document.createElement("button");
+  button.innerHTML = "-";
+  button.onclick = deletesocial;
+  socialContainer.appendChild(button);
+  document.getElementById("socialsList").appendChild(socialContainer);
 }
 
 function save() {
@@ -30,11 +35,22 @@ function save() {
 }
 
 function addsocial() {
-  let obj = document.createElement("p");
-  obj.innerHTML =
-    document.getElementById("socialName").value + " " + 
+  let socialContainer = document.createElement("div");
+  socialContainer.innerHTML =
+    document.getElementById("socialName").value +
+    " " +
     document.getElementById("socialUsername").value;
-  document.getElementById("socialsList").appendChild(obj);
+
+  let button = document.createElement("button");
+  button.innerHTML = "-";
+  button.onclick = deletesocial;
+  socialContainer.appendChild(button);
+  document.getElementById("socialsList").appendChild(socialContainer);
+}
+
+function deletesocial() {
+    parentnode = this.parentNode;
+    parentnode.parentNode.removeChild(parentnode);
 }
 
 function edit() {
