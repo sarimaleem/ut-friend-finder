fe988ef8617f0d04dabbaa3cf32ff16762b171ec
Abinavraj5427 <abinavraj10@gmail.com>
2021-04-21 18:43:06 -0500

    Added profile pics and readable content

--- frontend/search.js
+++ frontend/search.js
@@ -1,4 +1,4 @@
-var hey = {
+var userData = {
     "status": "pass",
     "users": [
       {
@@ -34,7 +34,7 @@ var hey = {
     ]
   }
 
-  var x = "<table>" //create the table
+  // var x = "<table>" //create the table
   /* <tr>
           <th>ID</th>
           <th>NAME</th>
@@ -52,20 +52,64 @@ var hey = {
         </tr> */
 
   //add the header (labels)
-  x += "<tr><th>ID</th><th>NAME</th><th>PASSWORD</th><th>EMAIL</th><th>SCHOOL EMAIL</th><th>DISTANCE</th><th>LOCATION</th><th>MAJOR</th><th>BIO</th><th>IMAGE-LINK</th><th>CLASSIFICATION</th><th>BIRTHDATE</th><th>GENDER</th></tr>"
+  // x += "<tr><th>ID</th><th>NAME</th><th>PASSWORD</th><th>EMAIL</th><th>SCHOOL EMAIL</th><th>DISTANCE</th><th>LOCATION</th><th>MAJOR</th><th>BIO</th><th>IMAGE-LINK</th><th>CLASSIFICATION</th><th>BIRTHDATE</th><th>GENDER</th></tr>"
 
-  for(i in hey.users){
+var table = document.getElementById("srp-table");
+var allUsers = userData.users;
+for(i in allUsers){
+  var data = allUsers[i];
+  var userEntry = document.createElement("div");
+  userEntry.className = "user-entry";
+  var contentHldr = document.createElement("div");
+  contentHldr.className = "content-holder";
+  var imgHldr = document.createElement("div");
+  imgHldr.className = "img-holder";
+  var pfp = document.createElement("img");
+  pfp.src = "./assets/pfp.jpg";
+  var userName = document.createElement("h2");
+  userName.innerHTML = data["Name"];
+  var userBio = document.createElement("p");
+  userBio.innerHTML = data["Bio"];
+  var tagDiv = document.createElement("div");
+  tagDiv.className = "tags";
+  var majorTag = document.createElement("div");
+  majorTag.className = "tag";
+  var userMajor = document.createElement("p");
+  userMajor.innerHTML = data["Major"];
 
-    x += "<tr>" //for each user, create a new row
-    var user = hey.users[i];
-    var keys = Object.keys(user) //get list of all data stored (name, id, etc)
-
-    for(j in keys){
-      x += "<th>"+(user[keys[j]])+"</th>" //get the info for each key
-    }
-    x += "</tr>"
-  }
-  x += "</table>"
-
-  // document.getElementById("srp-table").innerHTML =  x; //append to the doc
+  majorTag.appendChild(userMajor);
+  tagDiv.appendChild(majorTag);
+  contentHldr.appendChild(userName);
+  contentHldr.appendChild(userBio);
+  contentHldr.appendChild(tagDiv);
+  imgHldr.appendChild(pfp);
+  userEntry.appendChild(imgHldr);
+  userEntry.appendChild(contentHldr);
+  table.appendChild(userEntry);
+}
   
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("floatBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
